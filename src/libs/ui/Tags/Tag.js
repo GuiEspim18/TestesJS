1ffb80814e1db20f7bf5e936dc4207a7f30e89e8
guilherme.monteiro.espim@gmail.com <guilherme.monteiro.espim@gmail.com>
2024-09-21 00:11:32 -0300

    formatMeasure

--- src/libs/ui/Tags/Tag.js
+++ src/libs/ui/Tags/Tag.js
@@ -45,35 +45,35 @@ class Tag {
 
     setBorder({width = 1, color, style = "solid", radius = 0, measure = "pixels"}) {
         if (radius > 0) {
-            this.tag.style.borderRadius = radius + "px";
+            this.tag.style.borderRadius = this.formatMeasure(radius, measure);
         }
         this.tag.style.border = `${style} ${width}px ${color}`;
     }
 
     setBorderLeft({width = 1, color, style = "solid", radius = 0}) {
         if (radius > 0) {
-            this.tag.style.borderRadius = radius + "px";
+            this.tag.style.borderRadius = this.formatMeasure(radius, measure);
         }
         this.tag.style.borderLeft = `${style} ${width}px ${color}`;
     }
 
     setBorderRight({width = 1, color, style = "solid", radius = 0}) {
         if (radius > 0) {
-            this.tag.style.borderRadius = radius + "px";
+            this.tag.style.borderRadius = this.formatMeasure(radius, measure);
         }
         this.tag.style.borderRight = `${style} ${width}px ${color}`;
     }
 
     setBorderTop({width = 1, color, style = "solid", radius = 0}) {
         if (radius > 0) {
-            this.tag.style.borderRadius = radius + "px";
+            this.tag.style.borderRadius = this.formatMeasure(radius, measure);
         }
         this.tag.style.borderTop = `${style} ${width}px ${color}`;
     }
 
     setBorderBottom({width = 1, color, style = "solid", radius = 0}) {
         if (radius > 0) {
-            this.tag.style.borderRadius = radius + "px";
+            this.tag.style.borderRadius = this.formatMeasure(radius, measure);
         }
         this.tag.style.borderBottom = `${style} ${width}px ${color}`;
     }
