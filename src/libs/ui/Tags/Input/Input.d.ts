a5c3ec777f4691fb1276655c1ab47cbf1ef5ddcb
GuiEspim18 <guilherme.monteiro.espim@gmail.com>
2024-11-11 12:00:52 -0300

    input d ts

---
+++ src/libs/ui/Tags/Input/Input.d.ts
@@ -0,0 +1,21 @@
+import Tag from "../Tag";
+
+declare class Input extends Tag {
+    constructor();
+    
+    public setReadOnly(readOnly: boolean): void;
+
+    public setDisabled(disabled: boolean): void;
+
+    public setRequired(required: boolean): void;
+
+    public setAutofocus(autofocus: boolean): void;
+
+    public setAutocomplete(autocomplete: string): void;
+
+    public setPattern(pattern: string): void;
+
+    public setPlaceholder(placeholder: string): void;
+}
+
+export default Input;
