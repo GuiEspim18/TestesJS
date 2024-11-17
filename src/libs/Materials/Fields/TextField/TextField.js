6496a7c273d868dd5ddf1c280a24bbb5e2807f45
GuiEspim18 <guilherme.monteiro.espim@gmail.com>
2024-11-17 18:22:09 -0300

    text field

--- src/libs/Materials/Fields/TextField/TextField.js
+++ src/libs/Materials/Fields/TextField/TextField.js
@@ -1,8 +1,10 @@
 import Div from "../../../ui/Tags/Div/Div.js";
+import Display from "../../../ui/Types/Display";
 
 class TextField extends Div {
     constructor() {
-
+        this.setDisplay(Display.FLEX);
+        this.setPadding(5);
     }
 }
 
