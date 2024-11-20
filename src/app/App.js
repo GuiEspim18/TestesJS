c097826a48b885d1c2f1de535f8e4084554e8382
guilherme.monteiro.espim@gmail.com <guilherme.monteiro.espim@gmail.com>
2024-11-20 10:12:04 -0300

    reset project

--- src/app/App.js
+++ src/app/App.js
@@ -1,27 +1,9 @@
 import Component from "../libs/ui/Component/Component.js";
-import HeaderStyle from "./HeaderStyle.js";
-
-import Main from "../libs/ui/Tags/Main/Main.js";
-import Measure from "../libs/ui/Types/Measure.js";
-import FormCard from "./FormCard.js";
 
 class App extends Component {
 
     constructor() {
         super();
-
-        this.setPageColor("#FOFOFO");
-        this.setFontFamily("arial");
-
-        const header = new HeaderStyle();
-        this.add(header);
-
-        const main = new Main();
-        main.setSize(100, Measure.PERCENT);
-
-        main.add(new FormCard());
-
-        this.add(main);
 
     }
 
