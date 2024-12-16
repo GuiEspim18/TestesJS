67aaf539b14253878705c418f3facdb2fa7f9ba8
GuiEspim18 <guilherme.monteiro.espim@gmail.com>
2024-12-16 15:32:13 -0300

    tabs

--- src/libs/Materials/Tabs/Tabs.js
+++ src/libs/Materials/Tabs/Tabs.js
@@ -2,6 +2,15 @@ import Div from "../../ui/Tags/Div/Div";
 
 class Tabs extends Div {
 
+    constructor(tabs = []) {
+        super();
+
+        if (tabs.length > 0) {
+            
+        } 
+
+    }
+
 }
 
 export default Tabs;
