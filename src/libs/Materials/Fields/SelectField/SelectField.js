7fdcdbe45df412a2d3a0b6b43063ca600ce4eb53
GuiEspim18 <guilherme.monteiro.espim@gmail.com>
2025-01-16 07:32:02 -0300

    select field

--- src/libs/Materials/Fields/SelectField/SelectField.js
+++ src/libs/Materials/Fields/SelectField/SelectField.js
@@ -1,8 +1,14 @@
 import Field from "../Field.js";
+import Select from "../../../ui/Tags/Input/Select/Select.js";
 
 class SelectField extends Field {
     constructor(text = "", name = "", placeholder = "") {
         super(text, name, placeholder);
+
+        this.input = new Select();
+        this.add(this.input);
+
+        this.loadStyles();
     }
 }
 
