dd12443c8779d7fe6015ffe69e6fef62f7ceb2bf
GuiEspim18 <guilherme.monteiro.espim@gmail.com>
2025-01-26 13:17:03 -0300

    email field

--- src/libs/Materials/Fields/EmailField/EmailField.js
+++ src/libs/Materials/Fields/EmailField/EmailField.js
@@ -3,6 +3,8 @@ import Field from "../Field.js";
 class EmailField extends Field {
     constructor(text = "", name = "", placeholder = "") {
         super(text, name, placeholder);
+
+        this.input = new InputNumber();
     }
 }
 
