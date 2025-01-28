b77c96f2a0f554ed19d05b9ffb90bee1060088cb
GuiEspim18 <guilherme.monteiro.espim@gmail.com>
2025-01-28 13:20:19 -0300

    email field

--- src/libs/Materials/Fields/EmailField/EmailField.js
+++ src/libs/Materials/Fields/EmailField/EmailField.js
@@ -1,10 +1,11 @@
 import Field from "../Field.js";
+import InputEmail from "../../../ui/Tags/Input/InputEmail/InputEmail.js";
 
 class EmailField extends Field {
     constructor(text = "", name = "", placeholder = "") {
         super(text, name, placeholder);
 
-        this.input = new InputNumber();
+        this.input = new InputEmail();
         this.holder.add(this.input);
 
         this.loadStyles();
