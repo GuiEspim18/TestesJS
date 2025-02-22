c8ac2bb127e01b04a15c95ee4668449726e3874b
GuiEspim18 <guilherme.monteiro.espim@gmail.com>
2025-02-22 15:06:02 -0300

    changes

--- src/libs/Materials/Fields/FieldType.js
+++ src/libs/Materials/Fields/FieldType.js
@@ -3,10 +3,7 @@ class FieldType {
     static TEXT = 2;
     static EMAIL = 3;
     static SELECT = 4;
-<<<<<<< HEAD
     static DATE = 5;
-=======
->>>>>>> 173ca611d482c2354135048d368627fdb983d995
 }
 
 export default FieldType;
